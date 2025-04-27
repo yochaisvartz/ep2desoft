1b5f1d98ab81b29a1487c76462ba999bd0e3e5ab
yochaisvartz <yochais@al.insper.edu.br>
2025-04-27 16:41:01 -0300

    exercicio 2

--- funcoes.py
+++ funcoes.py
@@ -6,3 +6,14 @@ def rolar_dados(n):
         numero = random.randint(1,6)
         listadados.append(numero)
     return listadados
+
+# exercicio 2
+def guardar_dado (listadados, dadosguardados, n):
+    dadosrolados = []
+    for i in range(len(listadados)):
+        if i != n:
+            dadosrolados.append(listadados[i])
+    dadoarmazenado = listadados[n]
+    dadosguardados.append(dadoarmazenado)
+    listastatus = [dadosrolados, dadosguardados]
+    return listastatus
